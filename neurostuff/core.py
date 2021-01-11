084a20c3548673b13c1180ed81d026f01022d279
Tal Yarkoni <tyarkoni@gmail.com>
2021-01-11 14:47:17 -0600

    WIP: attempt to route using connexion

--- neurostuff/core.py
+++ neurostuff/core.py
@@ -5,29 +5,45 @@ from flask_dance.consumer.storage.sqla import SQLAlchemyStorage
 from flask_dance.contrib.github import make_github_blueprint
 from flask_cors import CORS
 
+import connexion
+from connexion.resolver import MethodViewResolver
+
 from .database import init_db
 from .models import User, Role, OAuth
 
 
-app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
-db = init_db(app)
+app = connexion.FlaskApp(__name__, specification_dir="openapi/", debug=True)
+
+flask_app = app.app
+flask_app.config.from_object(os.environ["APP_SETTINGS"])
+db = init_db(flask_app)
+
+options = {"swagger_ui": True}
+app.add_api(
+    "api.yaml",
+    options=options,
+    arguments={"title": "NeuroStore API"},
+    resolver=MethodViewResolver("api"),
+    strict_validation=True,
+    validate_responses=True,
+)
 
 # Enable CORS
-cors = CORS(app, expose_headers='X-Total-Count')
+cors = CORS(flask_app, expose_headers="X-Total-Count")
 
 # Flask-Security
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
-security = Security(app, user_datastore)
+security = Security(flask_app, user_datastore)
 
 # Flask-Dance (OAuth)
 from . import oauth
-app.secret_key = app.config['DANCE_SECRET_KEY']
+
+flask_app.secret_key = flask_app.config["DANCE_SECRET_KEY"]
 blueprint = make_github_blueprint(
-    client_id=app.config['GITHUB_CLIENT_ID'],
-    client_secret=app.config['GITHUB_CLIENT_SECRET'],
+    client_id=flask_app.config["GITHUB_CLIENT_ID"],
+    client_secret=flask_app.config["GITHUB_CLIENT_SECRET"],
 )
-app.register_blueprint(blueprint, url_prefix="/login")
+flask_app.register_blueprint(blueprint, url_prefix="/login")
 blueprint.storage = SQLAlchemyStorage(OAuth, db.session)
 
 # # GraphQL API
@@ -37,6 +53,7 @@ blueprint.storage = SQLAlchemyStorage(OAuth, db.session)
 #                  'graphql',schema=graphql_schema, graphiql=True,
 #                  context_value={'session': db.session}))
 
-# Bind routes
-from .resources import bind_resources
-bind_resources(app)
+# # Bind routes
+# from .resources import bind_resources
+
+# bind_resources(app)
